f970947ce69cf5f8263437d1ae661ad8f66447fc
ForcePushEnjoyer <greatest.programming.enthusiast@gmail.com>
2024-02-22 16:38:22 +0100

    refactored class BinarySearchNode to make use of abstract class BasicTreeNode

--- src/main/java/org/data/structure/tree/bst/BinarySearchNode.java
+++ src/main/java/org/data/structure/tree/bst/BinarySearchNode.java
@@ -1,11 +1,10 @@
 package org.data.structure.tree.bst;
 
-public class BinarySearchNode<T> {
-    private T data;
-    private BinarySearchNode<T> leftNode;
-    private BinarySearchNode<T> rightNode;
+import org.data.structure.tree.BasicTreeNode;
+
+public class BinarySearchNode<T> extends BasicTreeNode<T> {
 
     public BinarySearchNode(T data) {
-        this.data = data;
+        super(data);
     }
 }
