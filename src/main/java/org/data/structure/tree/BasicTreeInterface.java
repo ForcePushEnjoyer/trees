30d72a41ad7209abab477e6e42346e2ca77a300b
ForcePushEnjoyer <greatest.programming.enthusiast@gmail.com>
2024-02-24 15:07:25 +0100

    new abstract class BasicTreeStructure replacing BasicTreeInterface

--- src/main/java/org/data/structure/tree/BasicTreeInterface.java
+++
@@ -1,8 +0,0 @@
-package org.data.structure.tree;
-
-public interface BasicTreeInterface<T> {
-    Boolean insert(T node);
-    Boolean contains(T node);
-    Boolean delete(T node);
-    Boolean isEmpty(T node);
-}
